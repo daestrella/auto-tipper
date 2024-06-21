1ed55074554cfada4bb403083d09776aa6ce50ad
daestrella <da.estrella@yahoo.com>
2024-06-21 17:31:19 +0800

    Added styling to output

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.getElementById("price").addEventListener("submit", function(event) {
 
     document.getElementById("submit").onclick = async() => {
 	let values = data.categories.map(category =>
-	    category.subcategories.reduce((acc, subcat) => acc + parseInt(document.getElementById(subcat.name).value), 0)
+	    category.subcategories.reduce((acc, subcat) => acc + parseFloat(document.getElementById(subcat.name).value), 0)
 	    / category.subcategories.length
 	);
 	console.log(values);
@@ -23,7 +23,12 @@ document.getElementById("price").addEventListener("submit", function(event) {
 	console.log(price);
 
 	document.querySelector('#output').innerHTML = `
-	    <p>&#8369;&nbsp;${Fuzzy.sugeno(generateRules(data.ratings, price, values)).toFixed(2)}</p>
+	    <div class="card my-5">
+		<div class="card-body">
+		    <p>Based on your answers, your suggested tip is</p>
+		    <h2>&#8369;&nbsp;${Fuzzy.sugeno(generateRules(data.ratings, price, values)).toFixed(2)}</h2>
+		</div>
+	    </div>
 	`;
     };
 })();
